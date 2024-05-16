d0961cc8e7acdce04fe8028cca4ed64a6e51f2a2
csm0062 <csm0062@naver.com>
2024-05-16 17:34:22 +0900

    240516_스터디 전 푸쉬

---
+++ JavaProgramming/src/_240516_homework/PrintNumber.java
@@ -0,0 +1,6 @@
+package _240516_homework;
+
+public interface PrintNumber {
+	
+
+}
---
+++ JavaProgramming/src/_240516_homework/_01_Basic.java
@@ -0,0 +1,10 @@
+package _240516_homework;
+
+public class _01_Basic {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
