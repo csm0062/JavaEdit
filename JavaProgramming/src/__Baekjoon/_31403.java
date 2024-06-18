96c9027063697f7e25caad8522c237593a90e74f
csm0062 <csm0062@naver.com>
2024-06-18 09:20:38 +0900

    백준 31403

---
+++ JavaProgramming/src/__Baekjoon/_31403.java
@@ -0,0 +1,19 @@
+package __Baekjoon;
+
+import java.util.Scanner;
+
+public class _31403 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		System.out.println("수를 입력하세요.");
+		int A = sc.nextInt();
+		int B = sc.nextInt();
+		int C = sc.nextInt();
+		
+		System.out.println(A + B - C);
+		
+	}
+
+}
