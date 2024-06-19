24f97e67d1bce6990d2651578061ffe74b028d7f
csm0062 <csm0062@naver.com>
2024-06-19 09:31:24 +0900

    백준

--- JavaProgramming/src/__Baekjoon/_31403.java
+++ JavaProgramming/src/__Baekjoon/_31403.java
@@ -7,13 +7,19 @@ public class _31403 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
+		
 		System.out.println("수를 입력하세요.");
 		int A = sc.nextInt();
 		int B = sc.nextInt();
 		int C = sc.nextInt();
 		
-		System.out.println(A + B - C);
+		int sum = A + B - C;
+		String str = String.valueOf(A) + String.valueOf(B);
+		int sumStr = Integer.parseInt(str) - C;
+//		int sumStr = Integer.parseInt("" + A + B) - C;
 		
+		System.out.println(sum);
+		System.out.println(sumStr);
 	}
 
 }
---
+++ JavaProgramming/src/__Baekjoon/_4949.java
@@ -0,0 +1,11 @@
+package __Baekjoon;
+
+public class _4949 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+
+	}
+
+}
