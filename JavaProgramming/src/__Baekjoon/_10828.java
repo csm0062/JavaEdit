10907e7d9e3291d5534b22d2bef79be7e6f2d61c
CHOISOOM\csm00 <csm0062@naver.com>
2024-06-22 21:59:44 +0900

    no message

--- JavaProgramming/src/__Baekjoon/_10828.java
+++ JavaProgramming/src/__Baekjoon/_10828.java
@@ -11,44 +11,45 @@ public class _10828 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
-
-		int[] stack = new int[?];
-		int size = 0;
 		
-		void push (int num) {
-			stack[size]= num;
-			size++;
-		}
 		
-		int pop() {
-			if(size==0) {
-				return -1;
-			} else {
-				int top = stack[size-1];
-				stack[size-1] = 0;
-				size--;
-				return top;
-			}
-		}
-		
-		int empty() {
-			if(size == 0) {
-				return 1;
-			} else {
-				return 0;
-			}
-		}
-		
-		int top() {
-			if(size == 0) {
-				return -1;
-			}
-		} else {
-			return stack[size - 1];
-		}
 		
         
 		
 	}
 
 }
+//int[] stack = new int[?];
+//int size = 0;
+//
+//void push (int num) {
+//	stack[size]= num;
+//	size++;
+//}
+//
+//int pop() {
+//	if(size==0) {
+//		return -1;
+//	} else {
+//		int top = stack[size-1];
+//		stack[size-1] = 0;
+//		size--;
+//		return top;
+//	}
+//}
+//
+//int empty() {
+//	if(size == 0) {
+//		return 1;
+//	} else {
+//		return 0;
+//	}
+//}
+//
+//int top() {
+//	if(size == 0) {
+//		return -1;
+//	}
+//} else {
+//	return stack[size - 1];
+//}
