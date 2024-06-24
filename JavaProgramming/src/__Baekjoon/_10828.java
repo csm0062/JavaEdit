9f772b3c19ee503f4bcdce24f01b3987d3613cab
CHOISOOM\csm00 <csm0062@naver.com>
2024-06-24 09:35:19 +0900

    240624_3차 백준

--- JavaProgramming/src/__Baekjoon/_10828.java
+++ JavaProgramming/src/__Baekjoon/_10828.java
@@ -1,6 +1,10 @@
 package __Baekjoon;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+import java.util.StringTokenizer;
 
 // 스택을 사용해서 푸는 문제
 
@@ -8,13 +12,43 @@ public class _10828 {
 	
 	
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
-		Scanner sc = new Scanner(System.in);
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		Stack<Integer> stack = new Stack<>();
+		StringBuilder sb = new StringBuilder();
+		int n = Integer.parseInt(br.readLine());
 		
-		
-		
-        
+		for(int i = 0; i < n; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			String s = st.nextToken();
+			
+			//push면 스텍에 x push
+			if (s.equals("push")) {
+                stack.push(Integer.parseInt(st.nextToken()));
+            } else if (s.equals("pop")) {
+                if (stack.isEmpty()) {
+                    sb.append(-1).append("\n");
+                } else {
+                    sb.append(stack.pop()).append("\n");
+                }
+            } else if (s.equals("size")) {
+                sb.append(stack.size()).append("\n");
+            } else if (s.equals("empty")) {
+                if (stack.isEmpty()) {
+                    sb.append(1).append("\n");
+                } else {
+                    sb.append(0).append("\n");
+                }
+            } else if (s.equals("top")) {
+                if (stack.isEmpty()) {
+                    sb.append(-1).append("\n");
+                } else {
+                    sb.append(stack.peek()).append("\n");
+                }
+            }
+        }
+        System.out.println(sb);
 		
 	}
 
