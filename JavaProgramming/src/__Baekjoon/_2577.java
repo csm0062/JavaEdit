eb5f9b3fd8cbe9685c9105c41c94280962bd6355
CHOISOOM\csm00 <csm0062@naver.com>
2024-07-12 19:11:53 +0900

    no message

--- JavaProgramming/src/__Baekjoon/_2577.java
+++ JavaProgramming/src/__Baekjoon/_2577.java
@@ -1,5 +1,7 @@
 package __Baekjoon;
 
+import java.util.Scanner;
+
 public class _2577 {
 
 	public static void main(String[] args) {
@@ -11,9 +13,22 @@ public class _2577 {
 //예를 들어 A = 150, B = 266, C = 427 이라면 A × B × C = 150 × 266 × 427 = 17037300 이 되고,
 //계산한 결과 17037300 에는 0이 3번, 1이 1번, 3이 2번, 7이 2번 쓰였다.
 		
+		Scanner sc = new Scanner(System.in);
 		
+		int num = (sc.nextInt() * sc.nextInt() * sc.nextInt());
+		String str = Integer.toString(num);
+		sc.close();
 		
-
+		for(int i = 0; i < 10; i++) {
+			int count = 0;
+			for(int j = 0; j < str.length(); j++) {
+				//여기서 '0'와 아스키코드 48이 같은값이라 원래 아스키코드 51이 3이기 때문에 -48을 해주던 -'0'을 해줘야한다.
+				if((str.charAt(j) - '0') == i) { 
+					count++;
+				}
+			}
+			System.out.println(count);
+		}
 	}
 
 }
